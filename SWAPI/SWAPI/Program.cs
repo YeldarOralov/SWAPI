71ba16f7e3bcc083e64212361987afa725d78a04
Eldar Oralov <eldar.oralov@gmail.com>
2019-03-20 17:08:43 +0600

    Fourth commit

--- SWAPI/SWAPI/Program.cs
+++ SWAPI/SWAPI/Program.cs
@@ -16,80 +16,89 @@ namespace SWAPI
     {
         static void Main(string[] args)
         {
-            XmlSerializer formatter = new XmlSerializer(typeof(Character));
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Character>));
             WebClient client = new WebClient();
             int idOfCharacter;
             string urlAddress;
-            bool checkCharacter = false;
+            bool checkCharacter = true;
+            List<Character> SWAPI = new List<Character>();
+
             Console.WriteLine("Введите Id персонажа:");
-                while (true)
-                {
-                    urlAddress = "https://swapi.co/api/people/";
-                    if (int.TryParse(Console.ReadLine(), out idOfCharacter))
-                    {
-                        urlAddress += idOfCharacter;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ошибка! Нужно ввести число.");
-                    }
-                }
-            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
-            xmlReaderSettings.ConformanceLevel = ConformanceLevel.Fragment;
-            using (XmlReader xr = XmlReader.Create("file.xml", xmlReaderSettings))
+            while (true)
             {
-                while (xr.Read())
+                urlAddress = "https://swapi.co/api/people/";
+                if (int.TryParse(Console.ReadLine(), out idOfCharacter))
                 {
-                    var newCharacter = (Character)formatter.Deserialize(xr);
-                    if (newCharacter.Url.Substring(0, newCharacter.Url.Length - 1) == urlAddress)
-                    {
-                        Console.WriteLine(
-                            $"{newCharacter.Name}\n" +
-                            $"{newCharacter.Height}\n" +
-                            $"{newCharacter.Mass}\n" +
-                            $"{newCharacter.Hair_color}\n" +
-                            $"{newCharacter.Skin_color}\n" +
-                            $"{newCharacter.Eye_color}\n" +
-                            $"{newCharacter.Birth_year}\n" +
-                            $"{newCharacter.Gender}\n" +
-                            $"{newCharacter.Homeworld}\n" +
-                            $"{newCharacter.Films}\n" +
-                            $"{newCharacter.Species}\n" +
-                            $"{newCharacter.Vehicles}\n" +
-                            $"{newCharacter.Starships}\n" +
-                            $"{newCharacter.Created}\n" +
-                            $"{newCharacter.Edited}\n" +
-                            $"{newCharacter.Url}\n");
-                        checkCharacter = true;
+                    urlAddress += idOfCharacter;
                     break;
-                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Нужно ввести число.");
                 }
             }
-            if (checkCharacter == false)
+            if (checkCharacter)
+            {
+                using (StreamReader sr = new StreamReader("file.xml"))
+                {
+                    SWAPI = (List<Character>)formatter.Deserialize(sr);
+                }
+                foreach (Character x in SWAPI)
+                {
+                    if (x.Url.Substring(0, x.Url.Length - 1) == urlAddress)
+                    {
+                        Console.WriteLine(
+                            $"{x.Name}\n" +
+                            $"{x.Height}\n" +
+                            $"{x.Mass}\n" +
+                            $"{x.Hair_color}\n" +
+                            $"{x.Skin_color}\n" +
+                            $"{x.Eye_color}\n" +
+                            $"{x.Birth_year}\n" +
+                            $"{x.Gender}\n" +
+                            $"{x.Homeworld}\n" +
+                            $"{x.Films}\n" +
+                            $"{x.Species}\n" +
+                            $"{x.Vehicles}\n" +
+                            $"{x.Starships}\n" +
+                            $"{x.Created}\n" +
+                            $"{x.Edited}\n" +
+                            $"{x.Url}\n");
+                        checkCharacter = false;
+                        break;
+                    }
+                }
+            }           
+            if (checkCharacter)
             {
                 var jsonChar = client.DownloadString(urlAddress);
-                var rez = (JsonConvert.DeserializeObject<Character>(jsonChar));
-                Console.WriteLine(
-                            $"{rez.Name}\n" +
-                            $"{rez.Height}\n" +
-                            $"{rez.Mass}\n" +
-                            $"{rez.Hair_color}\n" +
-                            $"{rez.Skin_color}\n" +
-                            $"{rez.Eye_color}\n" +
-                            $"{rez.Birth_year}\n" +
-                            $"{rez.Gender}\n" +
-                            $"{rez.Homeworld}\n" +
-                            $"{rez.Films}\n" +
-                            $"{rez.Species}\n" +
-                            $"{rez.Vehicles}\n" +
-                            $"{rez.Starships}\n" +
-                            $"{rez.Created}\n" +
-                            $"{rez.Edited}\n" +
-                            $"{rez.Url}\n");
+                SWAPI.Add(JsonConvert.DeserializeObject<Character>(jsonChar));
+                foreach (Character x in SWAPI)
+                {
+                    if (x.Url.Substring(0, x.Url.Length - 1) == urlAddress)
+                    {
+                        Console.WriteLine(
+                            $"{x.Name}\n" +
+                            $"{x.Height}\n" +
+                            $"{x.Mass}\n" +
+                            $"{x.Hair_color}\n" +
+                            $"{x.Skin_color}\n" +
+                            $"{x.Eye_color}\n" +
+                            $"{x.Birth_year}\n" +
+                            $"{x.Gender}\n" +
+                            $"{x.Homeworld}\n" +
+                            $"{x.Films}\n" +
+                            $"{x.Species}\n" +
+                            $"{x.Vehicles}\n" +
+                            $"{x.Starships}\n" +
+                            $"{x.Created}\n" +
+                            $"{x.Edited}\n" +
+                            $"{x.Url}\n");
+                    }
+                }
                 using (StreamWriter sw = new StreamWriter("file.xml"))
                 {
-                    formatter.Serialize(sw, rez, new XmlSerializerNamespaces(new XmlQualifiedName[] { new XmlQualifiedName(string.Empty) }));
+                    formatter.Serialize(sw, SWAPI, new XmlSerializerNamespaces(new XmlQualifiedName[] { new XmlQualifiedName(string.Empty) }));
                     Console.WriteLine("Объект сериализован");
                 }
             }
